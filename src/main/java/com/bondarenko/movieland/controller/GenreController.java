1357de19cf7a81ac6a869215a76c422353109b34
ValentynaBondarenko <valentynaBondarenkoo@gmail.com>
2023-11-26 23:32:19 +0200

    implemented logging

--- src/main/java/com/bondarenko/movieland/controller/GenreController.java
+++ src/main/java/com/bondarenko/movieland/controller/GenreController.java
@@ -3,6 +3,7 @@ package com.bondarenko.movieland.controller;
 import com.bondarenko.movieland.dto.ResponseGenreDTO;
 import com.bondarenko.movieland.service.GenreService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(path = "/api/v1/genre", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -20,6 +22,7 @@ public class GenreController {
 
     @GetMapping
     protected ResponseEntity<List<ResponseGenreDTO>> findAll() {
+        log.info("Received request to find all genres.");
         List<ResponseGenreDTO> response = genreService.getAllGenres();
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
--- src/main/java/com/bondarenko/movieland/controller/MovieController.java
+++ src/main/java/com/bondarenko/movieland/controller/MovieController.java
@@ -3,6 +3,7 @@ package com.bondarenko.movieland.controller;
 import com.bondarenko.movieland.dto.RequestMovieDTO;
 import com.bondarenko.movieland.service.MovieService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequiredArgsConstructor
 @RequestMapping(path = "/api/v1/movie", produces = MediaType.APPLICATION_JSON_VALUE)
@@ -21,20 +23,22 @@ public class MovieController {
 
     @GetMapping
     protected ResponseEntity<List<RequestMovieDTO>> findAll() {
+        log.info("Received request to find all movies.");
         List<RequestMovieDTO> response = movieService.findAllMovies();
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @GetMapping("/random")
     protected ResponseEntity<List<RequestMovieDTO>> getRandomMovie() {
+        log.info("Received request to find random movies.");
         List<RequestMovieDTO> response = movieService.getRandomMovies();
         return new ResponseEntity<>(response, HttpStatus.OK);
     }
 
     @GetMapping("/genre/{genreId}")
     public ResponseEntity<List<RequestMovieDTO>> getMoviesByGenre(@PathVariable int genreId) {
+        log.info("Received request to find movies by genre with ID {}.", genreId);
         List<RequestMovieDTO> movies = movieService.getMoviesByGenre(genreId);
         return ResponseEntity.ok(movies);
     }
-
 }
