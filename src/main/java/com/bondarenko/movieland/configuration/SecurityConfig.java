cf6dbb4a2033448e2e043facbd022b1e8eeadcd2
ValentynaBondarenko <valentynaBondarenkoo@gmail.com>
2025-05-22 17:19:12 +0300

    update security config

--- src/main/java/com/bondarenko/movieland/configuration/SecurityConfig.java
+++ src/main/java/com/bondarenko/movieland/configuration/SecurityConfig.java
@@ -3,23 +3,24 @@ package com.bondarenko.movieland.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public SecurityFilterChain filter(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity
-                .authorizeHttpRequests(auth -> auth
+    public SecurityFilterChain filter(HttpSecurity http) throws Exception {
+        return http
+                .authorizeHttpRequests((requests) -> requests
                         .requestMatchers(HttpMethod.POST, "/api/v1/movie").hasRole("ADMIN")
                         .requestMatchers(HttpMethod.PUT, "/api/v1/movie/{id}").hasRole("ADMIN")
                         .anyRequest().permitAll()
-                ).httpBasic()
-                .and()
-                .csrf().disable();
-
-        return httpSecurity.build();
+                )
+                .httpBasic(Customizer.withDefaults())
+                .csrf(AbstractHttpConfigurer::disable)
+                .build();
     }
 }
