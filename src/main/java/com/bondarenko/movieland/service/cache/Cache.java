e42a50f161c25cb7708649499c7b9588bc315164
ValentynaBondarenko <52756670+ValentynaBondarenko@users.noreply.github.com>
2025-07-27 21:47:05 +0300

    refactor : improve cache (#88)

--- src/main/java/com/bondarenko/movieland/service/cache/Cache.java
+++ src/main/java/com/bondarenko/movieland/service/cache/Cache.java
@@ -5,25 +5,44 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.locks.StampedLock;
 import java.util.function.Supplier;
 
 @Slf4j
 public class Cache<T> {
-    private final List<T> cache = new CopyOnWriteArrayList<>();
-    private final Supplier<List<T>> dbFetcher;
+    private final List<T> cache = new ArrayList<>();
+    private final Supplier<List<T>> dataFetcher;
+    private StampedLock lock = new StampedLock();
 
-    public Cache(Supplier<List<T>> dbFetcher) {
-        this.dbFetcher = dbFetcher;
+    public Cache(Supplier<List<T>> dataFetcher) {
+        this.dataFetcher = dataFetcher;
     }
 
     public List<T> getAll() {
-        return new ArrayList<>(cache);
+        long stamp = lock.tryOptimisticRead();
+        List<T> snapshot = new ArrayList<>(cache);
+        if (!lock.validate(stamp)) {
+            stamp = lock.readLock();
+            try {
+                snapshot = new ArrayList<>(cache);
+            } finally {
+                lock.unlockRead(stamp);
+                log.debug("Read lock released with stamp: {}", stamp);
+            }
+        }
+        return snapshot;
     }
 
     public void refresh() {
-        List<T> dataFromDb = dbFetcher.get();
-        addIfNotPresent(dataFromDb);
-        log.info("Cache updated, new size: {}", cache.size());
+        List<T> data = dataFetcher.get();
+        long stamp = lock.writeLock();
+        try {
+            addIfNotPresent(data);
+            log.info("Cache updated, new size: {}", cache.size());
+        } finally {
+            lock.unlockWrite(stamp);
+            log.debug("Write lock released with stamp: {}", stamp);
+        }
     }
 
     public void addIfNotPresent(List<T> data) {
